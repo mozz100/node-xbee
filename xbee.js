a60e0922dae3a151f99257c6daecd423e75cde95
Jan Kolkmeier <jankolkmeier@gmail.com>
2012-03-18 13:50:32 +0100

    make packetToJson always return an object

--- xbee.js
+++ xbee.js
@@ -312,109 +312,96 @@ function packetToJS(packet) {
   // the array of bytes excludes the start bit and the length bits (these are not collected by the serial parser funciton)
 
   // So, the first byte in the packet is the frame type identifier.
+
+  var json = {
+    type: undefined,
+    bytes: packet
+  }
+
   if (packet[0]== exports.FT_AT_RESPONSE) {
-    return {
-      type: 'AT Response',
-      frameId: packet[1],
-      command: String.fromCharCode(packet[2]) + String.fromCharCode(packet[3]), // translate bytes back to ASCII
-      commandStatus: (packet[4] == 0) ? 'OK' : packet[4],
-      commandData: packet.slice(4),
-      bytes: packet
-    }
+    json.type: 'AT Response';
+    json.frameId: packet[1],
+    json.command: String.fromCharCode(packet[2]) + String.fromCharCode(packet[3]), // translate bytes back to ASCII
+    json.commandStatus: (packet[4] == 0) ? 'OK' : packet[4],
+    json.commandData: packet.slice(4),
   } else if (packet[0] == exports.FT_REMOTE_AT_RESPONSE) {
-    return {
-      type: 'Remote AT Response',
-      frameId: packet[1],
-      remote64: {dec: packet.slice(2,10),  hex: byteArrayToHexString(packet.slice(2,10))},
-      remote16: {dec: packet.slice(10,12), hex: byteArrayToHexString(packet.slice(10,12))},
-      command: String.fromCharCode(packet[12]) + String.fromCharCode(packet[13]),
-      commandStatus: (packet[14] == 0) ? 'OK' : packet[14],
-      commandData: packet.slice(15),
-      bytes: packet
-    }
+    json.type: 'Remote AT Response',
+    json.frameId: packet[1],
+    json.remote64: {dec: packet.slice(2,10),  hex: byteArrayToHexString(packet.slice(2,10))},
+    json.remote16: {dec: packet.slice(10,12), hex: byteArrayToHexString(packet.slice(10,12))},
+    json.command: String.fromCharCode(packet[12]) + String.fromCharCode(packet[13]),
+    json.commandStatus: (packet[14] == 0) ? 'OK' : packet[14],
+    json.commandData: packet.slice(15),
   } else if(packet[0] == exports.FT_RECEIVE_RF_DATA) {
-    p = {
-      type: 'RF Data',
-      remote64: {dec: packet.slice(1,9),  hex: byteArrayToHexString(packet.slice(1,9))},
-      remote16: {dec: packet.slice(9,11), hex: byteArrayToHexString(packet.slice(9,11))},
-      receiveOptions: packet[11],
-      raw_data: packet.slice(12),
-      data: "",
-      bytes: packet
+    json.type: 'RF Data',
+    json.remote64: {dec: packet.slice(1,9),  hex: byteArrayToHexString(packet.slice(1,9))},
+    json.remote16: {dec: packet.slice(9,11), hex: byteArrayToHexString(packet.slice(9,11))},
+    json.receiveOptions: packet[11],
+    json.raw_data: packet.slice(12),
+    json.data: "",
+    for(i in json.raw_data) {
+      json.data += String.fromCharCode(json.raw_data[i]);
     }
-    // build ascii from raw_data
-    for(i in p.raw_data) {
-      p.data += String.fromCharCode(p.raw_data[i]);
-    }
-    return p
   } else if (packet[0] == exports.FT_DATA_SAMPLE_RX) {
-    s = {
-      type: 'Data Sample',
-      remote64: {dec: packet.slice(1,9),  hex: byteArrayToHexString(packet.slice(1,9))},
-      remote16: {dec: packet.slice(9,11), hex: byteArrayToHexString(packet.slice(9,11))},
-      receiveOptions: packet[11],
-      numSamples: packet[12],     // apparently always set to 1
-      digitalChannelMask: packet.slice(13,15),
-      analogChannelMask: packet[15],
-      bytes: packet
-    }
+    json.type: 'Data Sample',
+    json.remote64: {dec: packet.slice(1,9),  hex: byteArrayToHexString(packet.slice(1,9))},
+    json.remote16: {dec: packet.slice(9,11), hex: byteArrayToHexString(packet.slice(9,11))},
+    json.receiveOptions: packet[11],
+    json.numSamples: packet[12],     // apparently always set to 1
+    json.digitalChannelMask: packet.slice(13,15),
+    json.analogChannelMask: packet[15],
     // Bit more work to do on an I/O data sample.
     // First check s.digitalChannelMask - are there any digital samples?
-    if (s.digitalChannelMask[0] + s.digitalChannelMask[1] > 0) {
+    if (json.digitalChannelMask[0] + json.digitalChannelMask[1] > 0) {
       // digital channel mask indicates that digital samples are present, so they
       // are in the bytes 16 and 17.
-      s.digitalSamples = packet.slice(16,18);
+      json.digitalSamples = packet.slice(16,18);
       // Now check whether any analog samples are present
-      if (s.analogChannelMask > 0) {
-        s.analogSamples = packet.slice(18);
+      if (json.analogChannelMask > 0) {
+        json.analogSamples = packet.slice(18);
       }
     } else {
       // no digital samples.  There might still be analog samples...
-      if (s.analogChannelMask > 0) {
-        s.analogSamples = packet.slice(16);
+      if (json.analogChannelMask > 0) {
+        json.analogSamples = packet.slice(16);
       }
     }
 
     // translate digital samples into JS for easier handling
-    s['samples'] = {}
-
-    if (s.digitalChannelMask[0] + s.digitalChannelMask[1] > 0) {  // if digital samples present,
+    json.samples = {}
+    if (json.digitalChannelMask[0] + json.digitalChannelMask[1] > 0) {  // if digital samples present,
       // run through the first bitmask for digital pins, i.e. digiPinsByte1
       for (x in digiPinsByte1) {
         // On first iteration, for example, x = 'D10', digiPinsByte1[x] = 4.
         // OK.  So, is there a sample for this pin?  Check the digital channel mask.
-        if (s.digitalChannelMask[0] & digiPinsByte1[x]) {
+        if (json.digitalChannelMask[0] & digiPinsByte1[x]) {
           // There is a sample for this pin.  So, AND the sample byte and the bitmask,
           // and turn the result into a boolean.
           // On the first iteration, for example, this sets s['D10'] = 1
           // if the bitwise AND of the first byte of the digital sample with 4 is > 0
-          s['samples'][x] = ((s.digitalSamples[0] & digiPinsByte1[x]) > 0) ? 1 : 0;
+          json.samples[x] = ((json.digitalSamples[0] & digiPinsByte1[x]) > 0) ? 1 : 0;
         }
       }
       // do the same thing for the second load of digital inputs
       for (x in digiPinsByte2) {
-        if (s.digitalChannelMask[1] & digiPinsByte2[x]) {
-          s['samples'][x] = ((s.digitalSamples[1] & digiPinsByte2[x]) > 0) ? 1 : 0;
+        if (json.digitalChannelMask[1] & digiPinsByte2[x]) {
+          json.samples[x] = ((json.digitalSamples[1] & digiPinsByte2[x]) > 0) ? 1 : 0;
         }
       }
     }
 
     // Also translate analog samples into JS
     // The analog channel mask indicates which pins are enabled as analog channels.
-    if (s.analogChannelMask > 0) {
+    if (json.analogChannelMask > 0) {
       var sampleIndex = 0;
       for (x in analogPins) {
         // on first iteration, for example, x = 'A0', analogPins[x] = 1
-        if (s.analogChannelMask & analogPins[x]) {
-          s['samples'][x] = 256*s.analogSamples[sampleIndex*2]+s.analogSamples[1+sampleIndex*2];
+        if (json.analogChannelMask & analogPins[x]) {
+          json.samples[x] = 256*json.analogSamples[sampleIndex*2]+json.analogSamples[1+sampleIndex*2];
           sampleIndex += 1;
         }
       }
     }
-    return s;
-  } else {
-    // The first byte of the packet indicates it's an as-yet unknown frame type.
-    // In this case, just return the bytes.
-    return packet;  
   }
+  return packet;  
 }
