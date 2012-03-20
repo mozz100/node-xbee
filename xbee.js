b8e36e7b4d45a3f5db5d02821d7ba269d6708989
Jan Kolkmeier <jouzar@googlemail.com>
2012-03-20 08:40:23 +0100

    rewrote abstraction

--- xbee.js
+++ xbee.js
@@ -1,143 +1,172 @@
 var util = require('util');
-var async = require('async');
+var EventEmitter = require('events').EventEmitter;
+var api = require("./xbee-api");
 var serialport = require("serialport");
-exports.XBee = require("./xbee");
+var async = require('async');
 
-exports.open = function(port, cb) {
-  var xbee = {};
-  xbee._cb = undefined;
+function XBee(port) {
+  EventEmitter.call(this);
 
-  // execute an AT command on the local xbee module
-  xbee.AT = function(cmd, val, cb) {
-    var atc = new exports.XBee.ATCommand();
-    atc.setCommand(cmd);
-    if (typeof val === 'function') cb = val;
-    else if (val) atc.commandParameter = val;
-    b = atc.getBytes();
-    if (xbee._cb == undefined) {
-      xbee.serial.write(b);
-      if (typeof cb === 'function') xbee._cb = cb;
+  this.nodes = {};
+
+  this.serial = new serialport.SerialPort(port, { 
+    parser: api.packetParser()
+  });
+
+  var self = this;
+
+  this._onNodeDiscovery = function(node) {
+    // RemoveAllListeners??ß
+    if (self.nodes[node.remote64.hex]) {
+      self.nodes[node.remote64.hex].removeAllListeners();
     } else {
-      if (cb) cb(false);
-      console.log("ERROR: XBee occupied");
+      self.nodes[node.remote64.hex] = new Node(self, node);
     }
-  };
-  
-  xbee.send = function(payload, remote64, remote16) {
-    var trf = new exports.XBee.TransmitRFData();
-    trf.destination64 = remote64;
-    trf.destination16 = remote16;
-    trf.RFData = payload;
-    b = trf.getBytes();
-    if (true || xbee._cb == undefined) {
-      xbee.serial.write(b);
-    } else {
-      console.log("ERROR: XBee occupied");
-    }
+    this.emit("node", self.nodes[node.remote64.hex]);
   }
-  
-  xbee.RemoteAT = function(cmd, remote64, remote16, val, cb) {
-    var atc = new exports.XBee.RemoteATCommand();
-    atc.setCommand(cmd);
-    if (typeof val === 'function') cb = val;
-    else if (val) atc.commandParameter = val;
-    atc.destination64 = remote64;
-    atc.destination16 = remote16;
-    b = atc.getBytes();
-    if (xbee._cb == undefined) {
-      xbee.serial.write(b);
-      if (typeof cb === 'function') xbee._cb = cb;
+
+  // On AT Response
+  this._onRemoteATResponse = function(res) {
+    // On Node Discovery Packet, emit new Node
+    if (self.nodes[res.remote64.dec]) {
+      self.nodes[res.remote64.dec]._onRemoteATResponse(res);
     } else {
-      if (cb) cb(false);
-      console.log("ERROR: XBee occupied");
+      console.log("Unhandled REMOTE_AT_RESPONSE: %s", util.inspect(res));
     }
   }
 
-  xbee.serial = new serialport.SerialPort(port, { 
-    parser: exports.XBee.packetParser()
-  });
+  this._onATResponse_FilterND = function(res) {
+    if (res.command == "ND") self._onNodeDiscovery(res.node);
+  }
 
-  var at_cb = function(data) {
-    //console.log(util.inspect(data));
-    if (data.command == "ND") {
-      console.log("ATND RES: "+util.inspect(data.node));
-    } else if (xbee._cb != undefined) {
-      var cb = xbee._cb;
-      xbee._cb = undefined;
-      cb(data);
+  this._onMessage = function(data) {
+    if (self.nodes[data.remote64.hex]) {
+      self.nodes[data.remote64.hex]._onData(data);
     } else {
-      console.log("UNHANDLED "+util.inspect(data));
+      console.log("ERROR: Data from unknown node!");
     }
   }
-  xbee.serial.on("REMOTE_AT_RESPONSE", at_cb);
-  xbee.serial.on("AT_RESPONSE", at_cb);
-  xbee.serial.on("RECEIVE_RF_DATA", function(data) {
-    console.log("> "+data.data);
-  });
 
-  xbee.serial.on("NODE_DENTIFICATION", function(node) {
-    console.log(node.nodeIdentifier+" is online");  
-    xbee.send("Hey! Nice you're here!\n", node.remote64.dec, node.remote16.dec);
-    /*
-    if (node.nodeIdentifier === "NODE2") {
-      var spam = function() {
-        xbee.RemoteAT('ID', node.remote64.dec, node.remote16.dec, function(data) {
-          console.log("REMOTE_AT_RESPONSE: "+data.commandStatus);
-        });
-      }
-    } else {
-      var spam = function() {
-      }
-    }
-    */
-    
-  });
+  this.serial.on("REMOTE_AT_RESPONSE", this._onRemoteATResponse);
+  this.serial.on("NODE_IDENTIFICATION", this._onNodeDiscovery);
+  this.serial.on("RECEIVE_RF_DATA", this._onMessage);
 
+  this.configure();
+}
+
+util.inherits(XBee, EventEmitter);
+
+XBee.prototype.configure = function() {
+  var self = this;
   var QF = function(command, f) {
     f = typeof f !== 'undefined' ? f : function(a){return a};
     return function(cb) {
-      xbee.AT(command, function(data) {
+      self._ATCB(command, function(data) {
         cb(!data.commandStatus, f(data.commandData)); 
       });
     }
   }
 
   var config = {
-    panid: QF('ID', exports.XBee.bArr2HexStr),
-    identifier: QF('NI', exports.XBee.bArr2Str),
-    sourceLow: QF('SL', exports.XBee.bArr2HexStr),
-    sourceHigh: QF('SH', exports.XBee.bArr2HexStr)
+    panid:      QF('ID', api.bArr2HexStr),
+    id:         QF('NI', api.bArr2Str),
+    sourceLow:  QF('SL', api.bArr2HexStr),
+    sourceHigh: QF('SH', api.bArr2HexStr),
+    nodeDiscoveryTime: QF('NT', function(a) { return parseInt(a[0])*100; })
   };
   
   async.series(config, function(err, results) {
     console.log(results);
-    xbee.config = results;
-    xbee.AT('ND', function(data) {
-      console.log(util.inspect(data));
+    self.config = results;
+    self.emit("configured", self.config);
+    self.discover(function() {
+      console.log("Discovery Over");
     });
-    //var remote64 = [0x00,0x13,0xa2,0x00,0x40,0x33,0x02,0x87];
-    //var remote16 = [0x0e,0xdf]; 
-    //var remote64 = [0x00,0x00,0x00,0x00,0x00,0x00,0xff,0xff];
-    //var remote16 = [0xff,0xfe]; 
-    //xbee.RemoteAT('FR', remote64, remote16, function(data) {
-      //xbee.send("Hello World\n", remote64, remote16);
-      //console.log(util.inspect(data));
-    //});
-    //});
-    cb(xbee);
   });
-
 }
 
-/*
-// execute an AT command on a remote xbee module
+XBee.prototype.discover = function(cb) {
+  this._AT('ND');
+  this.serial.on("AT_RESPONSE", this._onATResponse_FilterND);
+  setTimeout(function() {
+    cb(); 
+  }, this.config.nodeDiscoveryTime);
+}
 
-/*
-// simple example: ATID on local xbee module
-AT('ID');
-// simple example: query ATD0 on remote xbee module.
-var remote64 = [0x00,0x13,0xa2,0x00,0x40,0x7a,0x1f,0x95];  // <-- you'll need to replace this with the 64-bit hex address of your module
-var remote16 = [0xff,0xfe]; // <-- put the 16 bit address of remote module here, if known. Otherwise use [0xff, 0xfe]
+XBee.prototype.broadcast = function(data) {
+  var remote64 = [0x00,0x00,0x00,0x00,0x00,0x00,0xff,0xff];
+  var remote16 = [0xff,0xfe]; 
+  this._send(data, remote64, remote16);
+}
 
-RemoteAT('D0', null, remote64, remote16);
-*/
+XBee._send = function(data, remote64, remote16) {
+  var frame = new api.TransmitRFData();
+  if (typeof remote64.dec === 'undefined') {
+    frame.destination64 = remote64;
+    frame.destination16 = remote16;
+  } else {
+    frame.destination64 = remote64.dec;
+    frame.destination16 = remote16.dec;
+  }
+  frame.RFData = data;
+  this.serial.write(frame.getBytes());
+}
+
+XBee.prototype._ATCB = function(cmd, val, cb) {
+  if (typeof val === 'function') {
+    cb = val;
+    val = undefined;
+  }
+  this._AT(cmd, val);
+  this.serial.on("AT_RESPONSE", function(res) {
+    if (res.command == cmd) {
+      cb(res);
+    }
+  });
+}
+
+XBee.prototype._AT = function(cmd, val) {
+  var frame = new api.ATCommand();
+  frame.setCommand(cmd);
+  frame.commandParameter = val;
+  this.serial.write(frame.getBytes());
+}
+
+XBee.prototype._remoteAT = function(cmd, remote64, remote16, val) {
+  var frame = new api.ATCommand();
+  frame.setCommand(cmd);
+  frame.commandParameter = val;
+  if (typeof remote64.dec === 'undefined') {
+    frame.destination64 = remote64;
+    frame.destination16 = remote16;
+  } else {
+    frame.destination64 = remote64.dec;
+    frame.destination16 = remote16.dec;
+  }
+  this.serial.write(frame.getBytes());
+}
+
+exports.XBee = XBee;
+
+function Node(xbee, params) {
+  EventEmitter.call(this);
+  this.xbee = xbee;
+  this.id = params.id;
+  this.remote16 = params.remote16;
+  this.remote64 = params.remote64;
+}
+
+
+Node.prototype.send = function(data) {
+  this.xbee._send(data, this.remote64, this.remote16);
+}
+
+Node.prototype._AT = function(cmd, val) {
+  this.xbee._remoteAT(cmd, this.remote64, this.remote16, val);
+}
+
+Node.prototype._onATResponse = function(res) {
+  console.log("Node %s got AT_RESPONSE: %s", util.inspect(res));
+}
+
+util.inherits(Node, EventEmitter);
